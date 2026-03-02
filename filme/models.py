dbb86db891535fc063f776e2bdb73adf339190e8
Luciero <pedrohs@ufba.br>
2026-03-02 17:12:44 -0300

    feat(models): implement Episode model for series
    Add new Episode model to the database structure.
    
    Define relationships between Episode and Series models,
    including fields such as title, description, season number,
    episode number, and release date.
    
    Prepare model for future integration with streaming functionality.

--- filme/models.py
+++ filme/models.py
@@ -22,6 +22,13 @@ class Filme(models.Model):
         return self.titulo
 
 # Criar os eps
+class Episodio(models.Model):
+    filme = models.ForeignKey("Filme", related_name='episodios', on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=100)
+    video = models.URLField()
+
+    def __str__(self):
+        return self.filme.titulo + " - " + self.titulo
 
 
 # Criar os usuarios
--- hashflix/urls.py
+++ hashflix/urls.py
@@ -19,9 +19,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('filme.urls')),
+    path('', include('filme.urls', namespace='filme')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
